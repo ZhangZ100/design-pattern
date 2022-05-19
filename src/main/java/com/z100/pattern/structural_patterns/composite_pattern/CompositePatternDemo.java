ab79887c16e0da6ad9b6b85d8c758940afdc2fe8
ZhangZ10000 <72861002+ZhangZ100@users.noreply.github.com>
2022-05-19 19:54:25 +0800

    [update] add composite pattern

---
+++ src/main/java/com/z100/pattern/structural_patterns/composite_pattern/CompositePatternDemo.java
@@ -0,0 +1,41 @@
+package com.z100.pattern.structural_patterns.composite_pattern;
+
+/**
+ * @author Z100
+ * @create 2022-05-19 19:48
+ * @desc composite pattern demo
+ **/
+public class CompositePatternDemo {
+    public static void main(String[] args) {
+        Employee CEO = new Employee("John","CEO", 30000);
+
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura","Marketing", 10000);
+        Employee clerk2 = new Employee("Bob","Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard","Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        //打印该组织的所有员工
+        //三层结构 CEO(head(staff,staff),head(staff,staff))
+        System.out.println(CEO);
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            System.out.println(headEmployee);
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
+    }
+}
